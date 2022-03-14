5e8f88ed15b653b9453653a61b1b1ecd9eb8f2bf
Shubham Kumar <54791621+nobita851@users.noreply.github.com>
2022-03-14 11:30:15 +0530

    improved Indentation

--- contracts/libraries/LibOpen.sol
+++ contracts/libraries/LibOpen.sol
@@ -291,27 +291,26 @@ library LibOpen {
 				uint256 newIndex = index + 1;
 				uint256 timeDiff = apy.time[newIndex] - time;
 				if(timeDiff > 600)
-				// Convert the aprChanges to the lowest unit value.
-				aggregateYield = (timeDiff * apy.apyChanges[index]);
+					aggregateYield = (timeDiff * apy.apyChanges[index]);
 			
 				for (uint256 i = newIndex; i < apy.apyChanges.length; i++) {
 					timeDiff = apy.time[i + 1] - apy.time[i];
 					if(timeDiff > 600)
-					aggregateYield += (timeDiff*apy.apyChanges[newIndex]);
+						aggregateYield += (timeDiff*apy.apyChanges[newIndex]);
 				}
 			}
 			else if (apy.time[index] == time) {
 				for (uint256 i = index; i < apy.apyChanges.length; i++) {
 					uint256 timeDiff = apy.time[i + 1] - apy.time[i];
 					if(timeDiff > 600)
-					aggregateYield += (timeDiff*apy.apyChanges[index]);
+						aggregateYield += (timeDiff*apy.apyChanges[index]);
 				}
 			}
 		} else if (apy.time.length == oldLengthAccruedYield && block.timestamp > time) {
 			if (apy.time[index] < time || apy.time[index] == time) {
 				uint256 timeDiff = block.timestamp - time;
 				if(timeDiff > 600)
-				aggregateYield += (timeDiff*apy.apyChanges[index]);
+					aggregateYield += (timeDiff*apy.apyChanges[index]);
 			}
 		}
 		interestFactor = aggregateYield ;
@@ -392,26 +391,26 @@ library LibOpen {
 				uint256 newIndex = index + 1;
 				uint timeDiff = apr.time[newIndex] - time;
 				if(timeDiff > 600)
-				aggregateYield = (timeDiff *apr.aprChanges[index]);
+					aggregateYield = (timeDiff *apr.aprChanges[index]);
 			
 				for (uint256 i = newIndex; i < apr.aprChanges.length; i++) {
 					timeDiff = apr.time[i + 1] - apr.time[i];
 					if(timeDiff > 600)
-					aggregateYield += (timeDiff*apr.aprChanges[newIndex]);
+						aggregateYield += (timeDiff*apr.aprChanges[newIndex]);
 				}
 			}
 			else if (apr.time[index] == time) {
 				for (uint256 i = index; i < apr.aprChanges.length; i++) {
 					uint256 timeDiff = apr.time[i + 1] - apr.time[i];
 					if(timeDiff > 600)
-					aggregateYield += (timeDiff*apr.aprChanges[index]);
+						aggregateYield += (timeDiff*apr.aprChanges[index]);
 				}
 			}
 		} else if (apr.time.length == oldLengthAccruedYield && block.timestamp > time) {
 			if (apr.time[index] < time || apr.time[index] == time) {
 				uint timeDiff = (block.timestamp - time);
 				if(timeDiff > 600)
-				aggregateYield += (timeDiff*apr.aprChanges[index]);
+					aggregateYield += (timeDiff*apr.aprChanges[index]);
 			}
 		}
 		interestFactor = aggregateYield ;
