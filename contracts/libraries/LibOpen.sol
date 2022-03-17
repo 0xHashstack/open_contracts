10b0345e3635d831603c951f664bc4af30df6ae4
trippdev.eth <shriyesh15@gmail.com>
2022-03-18 00:48:31 +0530

    removed _collateralPointer as it was redundant

--- contracts/libraries/LibOpen.sol
+++ contracts/libraries/LibOpen.sol
@@ -961,25 +961,27 @@ library LibOpen {
 		}
 	}
 
-	function _collateralPointer(address _account, bytes32 _loanMarket, bytes32 _commitment) internal view returns (bytes32, uint) {
-		AppStorageOpen storage ds = diamondStorage(); 
+	/// FUNCTION FOR _collateralPointer
+
+	// function _collateralPointer(address _account, bytes32 _loanMarket, bytes32 _commitment) internal view returns (bytes32, uint) {
+	// 	AppStorageOpen storage ds = diamondStorage(); 
 		
-		_hasLoanAccount(_account);
+	// 	_hasLoanAccount(_account);
 
-		// LoanRecords storage loan = ds.indLoanRecords[_account][_loanMarket][_commitment];
-		LoanState storage loanState = ds.indLoanState[_account][_loanMarket][_commitment];
-		CollateralRecords storage collateral = ds.indCollateralRecords[_account][_loanMarket][_commitment];
+	// 	// LoanRecords storage loan = ds.indLoanRecords[_account][_loanMarket][_commitment];
+	// 	LoanState storage loanState = ds.indLoanState[_account][_loanMarket][_commitment];
+	// 	CollateralRecords storage collateral = ds.indCollateralRecords[_account][_loanMarket][_commitment];
 
-		//require(loan.id !=0, "ERROR: No Loan");
-		require(loanState.state == STATE.REPAID, "ERROR: Active loan");
-		//if (_commitment != _getCommitment(0)) {
-		require((collateral.timelockValidity + collateral.activationTime) >= block.timestamp, "ERROR: Timelock in progress");
-		//}		
-		bytes32 collateralMarket = collateral.market;
-		uint collateralAmount = collateral.amount;
+	// 	//require(loan.id !=0, "ERROR: No Loan");
+	// 	require(loanState.state == STATE.REPAID, "ERROR: Active loan");
+	// 	//if (_commitment != _getCommitment(0)) {
+	// 	require((collateral.timelockValidity + collateral.activationTime) >= block.timestamp, "ERROR: Timelock in progress");
+	// 	//}		
+	// 	bytes32 collateralMarket = collateral.market;
+	// 	uint collateralAmount = collateral.amount;
 
-		return (collateralMarket, collateralAmount);
-	}
+	// 	return (collateralMarket, collateralAmount);
+	// }
 
 	function _accruedYieldCollateral(LoanAccount storage loanAccount, CollateralRecords storage collateral, CollateralYield storage cYield) internal {
 		bytes32 _commitment = cYield.commitment;
